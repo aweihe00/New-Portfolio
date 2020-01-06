a9a194be9ad64f97a20b49ce114e4cedfec9271c
Andrew Weihe <andrewy03@gmail.com>
2020-01-06 17:17:58 -0600

    hw/project links

---
+++ assets/portfolio.js
@@ -0,0 +1,26 @@
+const portfolio = [
+    {
+      title: "Project 1: Gamers Unite",
+      tagline: "An informative application about upcoming gaming conventions for Esport gamers",
+      url: "https://github.com/aweihe00/Project-1/",
+      type: "coding"
+    },
+    {
+        title: "My Portfolio",
+        tagline: "This very page by yours truly",
+        url: "https://github.com/aweihe00/Updated-Portfolio",
+        type: "coding"
+      },
+      {
+        title: "Code Quiz",
+        tagline: "A JavaScript Quiz, using...JavaScript",
+        url: "https://github.com/aweihe00/Code-Quiz/",
+        type: "coding"
+      },
+      {
+        title: "Weather Dashbaord",
+        tagline: "A simple weather dashboard utilizing APIs",
+        url: "https://github.com/aweihe00/Weather-Dashboard",
+        type: "coding"
+      },
+  ];
